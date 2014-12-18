e8eef8c6eab78d409d1ca965ce2c0bab6d2a3b32
XarpeSerpe <xarpeserpe@gmail.com>
2014-12-18 13:13:33 +0100

    Create robotSimple.ino
    Ejemplo de introducción que sirve para calibrar los motores.

---
+++ robotSimple.ino
@@ -0,0 +1,17 @@
+// Robot que avanza a media velocidad
+
+#include "DualMC33926MotorShield.h"
+
+DualMC33926MotorShield robot;
+
+void setup()
+{
+}
+
+void loop()
+{
+ robot.setSpeeds(200,200);
+ //Esta funcion admite valores entre -400 y 400 
+ //pero se debe limitar en funcion de la batria
+ // y del tipo de motor
+}
