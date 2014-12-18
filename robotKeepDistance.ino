fa95679a82aee4b53a594be7252c2e2e86bbc04e
XarpeSerpe <xarpeserpe@gmail.com>
2014-12-18 19:08:05 +0100

    Update robotKeepDistance.ino
    Añadida arreglo a la excepción de señal lejana. MAX_DISTANCE

--- robotKeepDistance.ino
+++ robotKeepDistance.ino
@@ -2,7 +2,7 @@
 #include <NewPing.h>
 #define TRIGGER_PIN  11  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define ECHO_PIN     6  // Arduino pin tied to echo pin on the ultrasonic sensor.
-#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+#define MAX_DISTANCE 50 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
 DualMC33926MotorShield robot;
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
@@ -16,7 +16,7 @@ void loop()
 {
  delay(50);
  unsigned int uS = sonar.ping();
- if (uS > 20)
+ if (uS < 20 || uS == 0)// Si la distancia es mayor de 50 cm, sonar.ping() devuelve 0
   {
     robot.setSpeeds(200,200);
   }
